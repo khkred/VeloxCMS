5b5794009d4c2d4e9ee9f3da799fadd185b683e5
khkred <khkrlife@gmail.com>
2021-03-29 19:00:41 +0530

    Added Date Picker

--- app/src/main/java/com/fsemicolon/veloxcms/DispatchActivity.java
+++ app/src/main/java/com/fsemicolon/veloxcms/DispatchActivity.java
@@ -2,7 +2,15 @@ package com.fsemicolon.veloxcms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class DispatchActivity extends AppCompatActivity {
 
@@ -10,5 +18,104 @@ public class DispatchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispatch);
+
+        //Let's get the Date Picker for dispatched Date
+
+
+        EditText dispatchDateEditText = findViewById(R.id.dispatch_date_edit_text);
+
+        dispatchDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                openDatePicker(dispatchDateEditText);
+            }
+        });
+
+
+        //Let's get the Time Picker for dispatched Time
+
+        EditText dispatchTimeEditText = findViewById(R.id.dispatch_time_edit_text);
+
+        dispatchTimeEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                openTimePicker(dispatchTimeEditText);
+            }
+        });
+
+
     }
+
+
+    /**
+     * The following method is used for getting the date picker
+     */
+
+    public void openDatePicker(EditText editText)
+    {
+
+        //First let's get the current date
+
+        Calendar calendar = Calendar.getInstance();
+
+        //From this calendar we will get year, month and day
+
+        int year = calendar.get(Calendar.YEAR);
+
+        int month = calendar.get(Calendar.MONTH);
+
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        //Since we have all of them let's open a datePickerDialog with today's date
+        DatePickerDialog mDatePickerDialog  = new DatePickerDialog(DispatchActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                //ONCE THE DATE IS SELECTED WE SHOULD SHOW IT IN OUR EDIT TEXT
+
+                String selectedDate = dayOfMonth+ "/" +(month+1)+ "/"+year;
+
+                editText.setText(selectedDate);
+            }
+        },year,month,dayOfMonth);
+
+        mDatePickerDialog.show();
+
+    }
+
+    /**
+     * The following method is used for getting the time picker
+     */
+
+    public void openTimePicker(EditText editText)
+    {
+
+        Calendar calendar = Calendar.getInstance();
+
+        //Let's get the current time from our Calendar
+
+        int hourOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        int minutes = calendar.get(Calendar.MINUTE);
+
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(DispatchActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                //TODO: FIGURE OUT TIME
+
+//                String currentTime = String.format("%02d:%02d",hourOfDay,minute);
+
+                editText.setText(hourOfDay+":"+minute);
+            }
+        },hourOfDay,minutes,false);
+
+
+        timePickerDialog.show();
+    }
+
 }
--- app/src/main/java/com/fsemicolon/veloxcms/MainActivity.java
+++ app/src/main/java/com/fsemicolon/veloxcms/MainActivity.java
@@ -2,18 +2,25 @@ package com.fsemicolon.veloxcms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     Button rSubmitButton;
 
     Spinner encodingSpinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +54,22 @@ public class MainActivity extends AppCompatActivity {
          * ==== END OF SPINNER CODE =====
          */
 
+        //Let's try to get the date picker when edit text is clicked
+
+
+        EditText receivedDateEditText = findViewById(R.id.received_date_edit_text);
+
+
+        receivedDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               openDatePicker(receivedDateEditText);
+            }
+        });
+
+
+
 
         rSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,4 +82,62 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /**
+     * The following method is used for getting the date picker
+     */
+
+
+    public void openDatePicker(EditText editText)
+    {
+
+
+        //First let's get the current date
+
+        Calendar calendar = Calendar.getInstance();
+
+        //From this calendar we will get year, month and day
+
+        int year = calendar.get(Calendar.YEAR);
+
+        int month = calendar.get(Calendar.MONTH);
+
+        int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+
+        //Since we have all of them let's open a datePickerDialog with today's date
+       DatePickerDialog mDatePickerDialog  = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                //ONCE THE DATE IS SELECTED WE SHOULD SHOW IT IN OUR EDIT TEXT
+
+                String selectedDate = dayOfMonth+ "/" +(month+1)+ "/"+year;
+
+                editText.setText(selectedDate);
+            }
+        },year,month,dayOfMonth);
+
+        mDatePickerDialog.show();
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
